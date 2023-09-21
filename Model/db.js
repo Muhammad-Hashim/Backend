df973b20c9a512e2658f4f1319109c9144a08202
Muhammad Hashim <muhammadhashimsardar@gmail.com>
2023-09-21 19:58:00 +0500

    refactor(env): :closed_lock_with_key: Add mongo password

--- Model/db.js
+++ Model/db.js
@@ -1,7 +1,8 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const mongoose = require("mongoose");
+const collactionkEY=process.env.MONGO_KEY
 const uri =
-  "mongodb+srv://muhammadhashimsardar:0ZPht1gvQnAiazY1@cluster0.fapwmzq.mongodb.net/note-app?retryWrites=true&w=majority";
+  `mongodb+srv://muhammadhashimsardar:${collactionkEY}.fapwmzq.mongodb.net/note-app?retryWrites=true&w=majority`;
 
 
 const connection = mongoose.connect(uri);
